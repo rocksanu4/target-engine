7c59fd44a18b4605b867f95c18e08e6871bb7d76
Anurag T <rocksanu4@gmail.com>
2025-07-14 13:18:18 +0530

    Added main changes for initializing backend

--- cmd/delivery/main.go
+++ cmd/delivery/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-kit/kit/tree/master/log"
 	"go.uber.org/zap"
 	"github.com/gorilla/mux"
 	_ "net/http/pprof"
@@ -24,19 +23,17 @@ func main() {
 	eps := endpoint.MakeDeliveryEndpoints(svc)
 	handler := transport.NewHTTPHandler(eps, logger)
 
-	// Prometheus metrics
 	metrics.RegisterPrometheusMetrics()
-
-	// pprof
-	go func() {
-		sugar.Debugln("pprof running on :6060")
-		http.ListenAndServe(":6060", nil)
-	}()
 
 	r := mux.NewRouter()
 	r.PathPrefix("/debug/pprof/").Handler(http.DefaultServeMux)
 	r.Handle("/metrics", metrics.GetPrometheusHandler())
 	r.PathPrefix("/").Handler(handler)
+
+	go func() {
+		sugar.Debugln("pprof running on :6060")
+		r.Handle(":6060", nil)
+	}()
 
 	sugar.Debugln("Service running on :8080")
 	sugar.Fatal(http.ListenAndServe(":8080", r))
