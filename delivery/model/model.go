5ac53c7e5908c9c541fe17b26bfd6a300e519986
Anurag T <rocksanu4@gmail.com>
2025-07-14 13:18:52 +0530

    Added model structs

--- delivery/model/model.go
+++ delivery/model/model.go
@@ -2,17 +2,32 @@ package model
 
 
 type Campaign struct {
-	CampaignID	string		`json:"campaignId"`
-	Name		string		`json:"name"`
-	Image		string		`json:"image"`
-	CTA			string		`json:"cta"`
-	Active		bool		`json:"active"`
+	ID               string   `json:"id"`
+	Status           string   `json:"status"` // ACTIVE, PAUSED, etc.
+	AppInclusion     []string `json:"app_inclusion,omitempty"`
+	AppExclusion     []string `json:"app_exclusion,omitempty"`
+	CountryInclusion []string `json:"country_inclusion,omitempty"`
+	CountryExclusion []string `json:"country_exclusion,omitempty"`
+	OSInclusion      []string `json:"os_inclusion,omitempty"`
+	OSExclusion      []string `json:"os_exclusion,omitempty"`
 }
 
-type TargetRule struct {
-
+type TargetingRule struct {
+    CampaignID string
+    Rules      []Rule
 }
 
+type Rule struct {
+    Dimension string
+    Type      string
+    Values    []string
+}
+
+type DeliveryRequest struct {
+    App     string `json:"app"`
+    Country string `json:"country"`
+    OS      string `json:"os"`
+}
 
 type Response struct {
 	Data		any			`json:"data"`
